80850c1e1521328ce6123a35ffbd0711c5f7b24c
bo-oseng <94548914+bo-oseng@users.noreply.github.com>
2022-05-12 03:07:49 +0900

    Update main.js

--- Movie seat booking/main.js
+++ Movie seat booking/main.js
@@ -4,21 +4,57 @@ const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 
+
+// Get data from localstorage and populate UI
+const populateUI = (() => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length !== 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            } 
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex  !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+});
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+
+// Save selected movie index and price
+const setMovieData = ((movieIndex) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+})
 
 
 // Update total and count
 const updateSelectCount = (() => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice;
 });
 
 
+
+
+
 // Movie select event
 movieSelect.addEventListener('change', event => {
     ticketPrice = +event.target.value;
+    setMovieData(event.target.selectedIndex);
     updateSelectCount();
 })
 
@@ -32,3 +68,8 @@ container.addEventListener('click', (event) => {
         updateSelectCount();
     }
 });
+
+
+// Initial count and total
+const initialCountAndTotal = (() => updateSelectCount());
+initialCountAndTotal();
